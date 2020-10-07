92a7902e807d6750e9f1cba9f2246fa084a68b8f
Andrew <a-kireev1989@mail.ru>
2020-10-08 02:08:08 +0300

    passed solution

--- main.cpp
+++ main.cpp
@@ -1,28 +1,47 @@
 #include <iostream>
+//В первой строке количество команд n. n ≤ 1000000.
+//Каждая команда задаётся как 2 целых числа: a b.
+//a = 1 - push front
+//        a = 2 - pop front
+//        a = 3 - push back
+//        a = 4 - pop back
+//        Команды добавления элемента 1 и 3 заданы с неотрицательным параметром b.
+//Для очереди используются команды 2 и 3. Для дека используются все четыре команды.
+//Если дана команда pop*, то число b - ожидаемое значение. Если команда pop вызвана для пустой структуры данных, то ожидается “-1”.
+//Формат выходных данных.
+//Требуется напечатать YES - если все ожидаемые значения совпали. Иначе, если хотя бы одно ожидание не оправдалось, то напечатать NO.
+
+//3_3. Реализовать очередь с помощью двух стеков. Использовать стек, реализованный с помощью динамического буфера.
+//Требования: Очередь должна быть реализована в виде класса. Стек тоже должен быть реализован в виде класса.
+
 
 template <typename T>
 class Stack {
 public:
-    Stack() : top_(nullptr), size_(0) {}
+    Stack() : size_(0), capacity_(5) {
+        top_ = new T[5];
+    }
 
     ~Stack() {
         delete [] top_;
     }
 
     void push(T value) {
-        if (size_ == 0) {
-            top_ = new T;
-            top_[0] = value;
-            ++size_;
-        } else {
-            T *old_top = top_;
-            top_ = new T[++size_];
-            for (int i = 0; i != size_ - 1; ++i) {
-                top_[i] = old_top[i];
-            }
-            top_[size_ - 1] = value;
-            delete[] old_top;
+        if (size_ == capacity_) {
+            resize();
         }
+            top_[size_] = value;
+            ++size_;
+    }
+
+    void resize() {
+        T *old_top = top_;
+        capacity_ *= 2;
+        top_ = new T[capacity_];
+        for (int i = 0; i != size_; ++i) {
+            top_[i] = old_top[i];
+        }
+        delete[] old_top;
     }
 
     void print_stack() const {
@@ -31,9 +50,12 @@ public:
         std::cout << std::endl;
     }
 
-    void pop() {
-        if (size_ > 0)
+    T pop() {
+        if (size_ > 0) {
             --size_;
+            return top_[size_];
+        }
+        return -1;
     }
 
     T top() const{
@@ -54,26 +76,84 @@ public:
 private:
     T *top_;
     size_t size_;
+    size_t capacity_;
+};
+
+template <typename T>
+class Queue {
+public:
+    Queue() {}
+
+    void push(T value) {
+        in_.push(value);
+    }
+
+    int pop() {
+        if (out_.size() != 0 || in_.size() != 0) {
+            if (out_.size() != 0)
+                return out_.pop();
+            else {
+                while (!in_.is_clear()) {
+                    out_.push(in_.pop());
+                }
+                return out_.pop();
+            }
+        }
+        return -1;
+    }
+
+    T top() {
+        if (out_.size() != 0 || in_.size() != 0) {
+            if (out_.size() != 0)
+                return out_.top();
+            else {
+                while (!in_.is_clear()) {
+                    out_.push(in_.top());
+                    in_.pop();
+                }
+                return out_.top();
+            }
+        }
+        return -1;
+    }
+
+    void is_clear() const {
+        return (in_.is_clear() && out_.is_clear());
+    }
+
+private:
+    Stack<T> in_;
+    Stack<T> out_;
 };
 
 
 
+
+
 int main() {
-    Stack<int> stack;
+    Queue<int> queue;
 
-    for (int i = 1; i != 10; ++i)
-        stack.push(i);
-    stack.print_stack();
-    std::cout << "size = " << stack.size() << std::endl;
+    int n = 0;
+    int first, second;
+    bool all_right = true;
 
-    for (int i = 1; i != 9; ++i) {
-        std::cout << stack.top() << " ";
-        stack.pop();
+    std::cin >> n;
+    for (int i = 0; i != n; ++i) {
+        std::cin >> first >> second;
+        if (first == 3)
+            queue.push(second);
+        else if (first == 2) {
+//            std::cout << "second = " << queue.top() << std::endl;
+            if (queue.pop() != second) {
+//                std::cout << "second = " << second << std::endl;
+                all_right = false;
+                break;
+            }
+        }
     }
-    std::cout << std::endl;
-
-    for (int i = 1; i != 9; ++i)
-        stack.pop();
-    stack.print_stack();
+    if (all_right)
+        std::cout << "YES";
+    else
+        std::cout << "NO";
     return 0;
 }
